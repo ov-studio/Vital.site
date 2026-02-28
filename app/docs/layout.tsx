48953d6cc7684466be630c11d42eb4ce5c71dd3a
A <89867985+ov-aviril@users.noreply.github.com>
2026-02-28 12:01:47 +0300

    Disable `collapsible` flag on doc sidebar

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -4,7 +4,11 @@ import { baseOptions } from '@/lib/layout.shared';
 
 export default function Layout({ children }: LayoutProps<'/docs'>) {
   return (
-    <DocsLayout tree={source.getPageTree()} {...baseOptions()}>
+    <DocsLayout
+      tree={source.getPageTree()}
+      {...baseOptions()}
+      sidebar={{ collapsible: false }}
+    >
       {children}
     </DocsLayout>
   );
