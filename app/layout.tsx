d63ec2af0cc708c4ef87b3a7b24091b17e0a4b8f
A <89867985+ov-aviril@users.noreply.github.com>
2026-03-09 01:14:44 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,30 @@
 import { RootProvider } from 'fumadocs-ui/provider/next';
 import './global.css';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Geist, Geist_Mono, Rajdhani } from 'next/font/google';
 
 const geist = Geist({
   subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
   variable: '--font-geist'
 });
 
 const geistMono = Geist_Mono({
   subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
   variable: '--font-geist-mono'
+});
+
+const rajdhani = Rajdhani({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-rajdhani'
 });
 
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
     <html
       lang="en"
-      className={`${geist.variable} ${geistMono.variable}`}
+      className={`${geist.variable} ${geistMono.variable} ${rajdhani.variable}`}
       suppressHydrationWarning
     >
       <body className="flex flex-col min-h-screen font-sans">
