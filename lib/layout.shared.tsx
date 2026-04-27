9315c0ac37edaf424164225c2d67c05d1fca83dc
A <89867985+ov-aviril@users.noreply.github.com>
2026-04-27 19:28:29 +0300

    Fixed search bar padding

--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -5,11 +5,14 @@ import { SidebarToggle } from '@/components/docs-sidebar';
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      title: <Brand size="xs" variant="full" href="/#"/>,
+      title: (
+        <div style={{ marginTop: '14px' }}>
+          <Brand size="xs" variant="full" href="/#" />
+        </div>
+      ),
       children: (
-        <div style={{ display: 'flex', flex: 1, justifyContent: 'flex-end', alignItems: 'center' }}>
+        <div style={{ display: 'flex', flex: 1, justifyContent: 'flex-end', alignItems: 'center', marginTop: '14px' }}>
           <SidebarToggle/>
-          <br/><br/>
         </div>
       ),
     },
