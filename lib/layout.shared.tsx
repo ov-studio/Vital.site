ef9dffc32637144981c2c34614d984154d5997f1
A <89867985+ov-aviril@users.noreply.github.com>
2026-05-12 11:32:10 +0300

    Update layout.shared.tsx

--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -1,6 +1,5 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
 import { Brand } from '@/components/brand';
-import { SidebarToggle } from '@/components/sidebar';
 
 export function baseOptions(): BaseLayoutProps {
   return {
@@ -9,12 +8,7 @@ export function baseOptions(): BaseLayoutProps {
         <div style={{ marginTop: '12px' }}>
           <Brand size="xs" variant="full" href="/#" />
         </div>
-      ),
-      children: (
-        <div style={{ display: 'flex', flex: 1, justifyContent: 'flex-end', alignItems: 'center', marginTop: '12px' }}>
-          <SidebarToggle/>
-        </div>
-      ),
+      )
     },
     themeSwitch: {
       enabled: false,
