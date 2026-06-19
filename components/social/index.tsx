614556fca3ebcac93e01dadb70a260b58b25d4a1
A <89867985+ov-aviril@users.noreply.github.com>
2026-06-19 12:05:25 +0300

    Update index.tsx

--- components/social/index.tsx
+++ components/social/index.tsx
@@ -3,16 +3,16 @@ import { icons } from '@/components/atoms/icons';
 import './index.css';
 
 export function Social() {
-    return (
-        <div className="social">
-            {Object.entries(config_site.info.social).map(([key, item]) => {
-                const Icon = icons[key as keyof typeof icons];
-                return (
-                    <a key={key} href={item.href} target="_blank" rel="noopener noreferrer" className="social-link" aria-label={item.label}>
-                        {Icon && <Icon className="social-icon"/>}
-                    </a>
-                );
-            })}
-        </div>
-    );
+  return (
+    <div className="social">
+      {Object.entries(config_site.info.social).map(([key, item]) => {
+        const Icon = icons[key as keyof typeof icons];
+        return (
+          <a key={key} href={item.href} target="_blank" rel="noopener noreferrer" className="social-link" aria-label={item.label}>
+            {Icon && <Icon className="social-icon" />}
+          </a>
+        );
+      })}
+    </div>
+  );
 }
