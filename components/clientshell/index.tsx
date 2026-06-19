dfcb5b7db8cf50e8e8ed439e02c3667c8542171b
A <89867985+ov-aviril@users.noreply.github.com>
2026-06-19 12:07:19 +0300

    Update index.tsx

--- components/clientshell/index.tsx
+++ components/clientshell/index.tsx
@@ -2,6 +2,6 @@
 import { Effect } from '@/components/clientshell/effect';
 
 export function ClientShell({ children }: { children: React.ReactNode }) {
-    Effect();
-    return <>{children}</>;
+  Effect();
+  return <>{children}</>;
 }
