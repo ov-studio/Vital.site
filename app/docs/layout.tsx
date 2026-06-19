faf2840b32ecc9c150725f6e81473cef0b2538dd
A <89867985+ov-aviril@users.noreply.github.com>
2026-06-19 16:25:30 +0300

    Update layout.tsx

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,4 +1,3 @@
-import './doc.css';
 import * as lib_source from '@/lib/source';
 import * as lib_layout from '@/lib/layout.shared';
 import * as component_overlay from '@/components/overlay';
@@ -6,7 +5,7 @@ import * as component_social from '@/components/social';
 import * as component_sidebar from '@/components/sidebar';
 import * as component_clientshell from '@/components/clientshell';
 import * as fumadocs_layout_notebook from 'fumadocs-ui/layouts/notebook';
-
+import './doc.css';
 
 export default function Layout({ children }: LayoutProps<'/docs'>) {
   const { nav, ...base } = lib_layout.baseOptions();
