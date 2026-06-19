e3771376eb4fc39b6efc4524974198779a2ed279
A <89867985+ov-aviril@users.noreply.github.com>
2026-06-19 14:30:32 +0300

    Update page.tsx

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,26 +1,26 @@
 import '../global.css';
-import { Overlay } from '@/components/overlay';
-import { Navbar } from '@/components/navbar';
-import { Footer } from '@/components/footer';
-import { Hero } from '@/components/home/hero';
-import { Features } from '@/components/home/features';
-import { Ethos } from '@/components/home/ethos';
-import { ClientShell } from '@/components/clientshell';
+import * as component_overlay from '@/components/overlay';
+import * as component_navbar from '@/components/navbar';
+import * as component_footer from '@/components/footer';
+import * as component_home_hero from '@/components/home/hero';
+import * as component_home_features from '@/components/home/features';
+import * as component_home_ethos from '@/components/home/ethos';
+import * as component_clientshell from '@/components/clientshell';
 
 export default function HomePage() {
   return (
-    <ClientShell>
-      <Overlay/>
-      <Navbar links={[
+    <component_clientshell.ClientShell>
+      <component_overlay.Overlay/>
+      <component_navbar.Navbar links={[
         { label: 'Features', href: '#features' },
         { label: 'Ethos', href: '#ethos' },
         { label: 'Documentations', href: '/docs' },
         { label: 'Roadmap', href: '/roadmap' }
       ]}/>
-      <Hero/>
-      <Features/>
-      <Ethos/>
-      <Footer/>
-    </ClientShell>
+      <component_home_hero.Hero/>
+      <component_home_features.Features/>
+      <component_home_ethos.Ethos/>
+      <component_footer.Footer/>
+    </component_clientshell.ClientShell>
   );
 }
