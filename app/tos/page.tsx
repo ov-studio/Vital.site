7f8ff54acdc80bfe18f00c24ddb512dff6e301f6
A <234397639+ov-kivi@users.noreply.github.com>
2026-07-24 18:00:02 +0300

    Update page.tsx

--- app/tos/page.tsx
+++ app/tos/page.tsx
@@ -14,7 +14,11 @@ export default function TOSPage() {
   return (
     <component_clientshell.ClientShell>
       <component_overlay.Overlay/>
-      <component_navbar.Navbar links={[]}/>
+      <component_navbar.Navbar links={[
+        { label: 'Vault', href: '/vault' },
+        { label: 'Documentations', href: '/docs' },
+        { label: 'Roadmap', href: '/roadmap' }
+      ]}/>
       <component_tos.TOS/>
       <component_footer.Footer/>
     </component_clientshell.ClientShell>
