1286c4d40ad02f658496ea23b313159fa43cd12e
A <89867985+ov-aviril@users.noreply.github.com>
2026-08-06 13:35:45 +0300

    Update footer.tsx

--- configs/footer.tsx
+++ configs/footer.tsx
@@ -6,26 +6,24 @@ export const Footer = [
   {
     heading: 'Sandbox',
     links: [
-      { label: 'Vault', href: '/vault' },
-      { label: 'Documentations', href: '/docs' },
-      { label: 'Roadmap', href: '/roadmap' },
-      { label: 'Contributing', href: '/docs/building' }
+      { label: 'Vault',          href: '/vault'         },
+      { label: 'Documentations', href: '/docs'          },
+      { label: 'Roadmap',        href: '/roadmap'       },
+      { label: 'Contributing',   href: '/docs/building' }
     ]
   },
   {
     heading: 'Resource',
-    links: [
-      { label: `${config_site.info.git.site.repo}`, href: `https://github.com/${config_site.info.git.site.user}/${config_site.info.git.site.repo}` },
-      { label: `${config_site.info.git.sandbox.repo}`, href: `https://github.com/${config_site.info.git.sandbox.user}/${config_site.info.git.sandbox.repo}` },
-      { label: `${config_site.info.git.kit.repo}`, href: `https://github.com/${config_site.info.git.kit.user}/${config_site.info.git.kit.repo}` },
-      { label: `${config_site.info.git.vault.repo}`, href: `https://github.com/${config_site.info.git.vault.user}/${config_site.info.git.vault.repo}` }
-    ]
+    links: Object.values(config_site.info.git).map(({ user, repo }) => ({
+      label: repo,
+      href:  `https://github.com/${user}/${repo}`
+    }))
   },
   {
     heading: 'Social',
-    links: Object.entries(config_site.info.social).map(([key, item]) => ({
-      label: item.label,
-      href: item.href,
+    links: Object.entries(config_site.info.social).map(([key, { label, href }]) => ({
+      label,
+      href,
       icon: key
     }))
   },
