03f307d4a01a1718189edc8eae072089888ab6d4
A <ovanisa5454@gmail.com>
2026-08-13 09:27:08 +0300

    Update roadmap.tsx

--- configs/roadmap.tsx
+++ configs/roadmap.tsx
@@ -133,8 +133,8 @@ export const Roadmap: RoadmapSection[] = build([
           { label:  '[Server] Command: info',                  status: 'completed' },
           { label:  '[Shared] Command: version',               status: 'completed' },
           { label:  '[Shared] Command: clear',                 status: 'completed' },
-          { label:  '[Server] Command: kick <id>',             status: 'pending' },
-          { label:  '[Server] Command: players',               status: 'pending' },
+          { label:  '[Server] Command: kick <id>',             status: 'pending'   },
+          { label:  '[Server] Command: players',               status: 'pending'   },
           { label:  '[Server] Command: refresh',               status: 'completed' },
           { label:  '[Server] Command: start <name>',          status: 'completed' },
           { label:  '[Server] Command: stop <name>',           status: 'completed' },
@@ -143,9 +143,9 @@ export const Roadmap: RoadmapSection[] = build([
           { label:  '[Server] Command: stop_all',              status: 'completed' },
           { label:  '[Server] Command: restart_all',           status: 'completed' },
           { label:  '[Server] Command: shutdown',              status: 'completed' },
-          { label:  '[Client] Command: connect <ip> <port>',   status: 'pending' },
-          { label:  '[Client] Command: disconnect',            status: 'pending' },
-          { label:  '[Client] Command: status',                status: 'pending' }
+          { label:  '[Client] Command: connect <ip> <port>',   status: 'pending'   },
+          { label:  '[Client] Command: disconnect',            status: 'pending'   },
+          { label:  '[Client] Command: status',                status: 'pending'   }
         ]
       },
       {
