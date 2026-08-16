7317a194dc10d44e6ace32f8054cd3bd44aa09d3
A <ovanisa5454@gmail.com>
2026-08-16 09:39:22 +0300

    Update route.ts

--- app/llms.mdx/docs/[[...slug]]/route.ts
+++ app/llms.mdx/docs/[[...slug]]/route.ts
@@ -11,7 +11,8 @@ export async function GET(_req: Request, { params }: RouteContext<'/llms.mdx/doc
 
   return new Response(await lib_source.getLLMText(page), {
     headers: {
-      'Content-Type': 'text/markdown'
+      'Content-Type': 'text/markdown',
+      'Cache-Control': 'public, max-age=3600, s-maxage=86400, stale-while-revalidate=604800'
     }
   });
 }
