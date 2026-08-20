3bfa56264be5b18513afefd8d7dfb0c93bd4920a
A <234397639+ov-kivi@users.noreply.github.com>
2026-08-20 12:21:55 +0300

    .

--- backend/next.config.ts
+++ backend/next.config.ts
@@ -1,14 +1,9 @@
 import type * as next from 'next';
-import *      as path from 'path';
 
 const ALLOWED_ORIGIN = 'https://vital-sandbox.com';
 
 const config: next.NextConfig = {
   reactStrictMode: true,
-  webpack(cfg) {
-    cfg.resolve.alias['@/shared'] = path.resolve(__dirname, '../shared');
-    return cfg;
-  },
   async headers() {
     return [
       {
