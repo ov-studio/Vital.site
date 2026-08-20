71588b1f2f4ad419cc292f5f3c5c779db4a7a68b
A <234397639+ov-kivi@users.noreply.github.com>
2026-08-20 15:04:46 +0300

    Update next.config.ts

--- backend/next.config.ts
+++ backend/next.config.ts
@@ -1,9 +1,11 @@
-import type * as next from 'next';
+import *      as config_site from './configs/site';
+import type * as next        from 'next';
 
 const ALLOWED_ORIGIN = 'https://vital-sandbox.com';
 
 const config: next.NextConfig = {
   reactStrictMode: true,
+
   async headers() {
     return [
       {
@@ -21,6 +23,15 @@ const config: next.NextConfig = {
         ]
       }
     ];
+  },
+
+  async redirects() {
+    return Object.entries(config_site.info.social).map(([key, { href }]) => ({
+      source:      '/:path*',
+      has:         [{ type: 'host' as const, value: `${key}.vital-sandbox.com` }],
+      destination: href.startsWith('http') ? href : `https://${href}`,
+      permanent:   false
+    }));
   }
 };
 
