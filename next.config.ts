6df8e7e4ec124d880e84a2a6762c41b5e4d58ec8
A <234397639+ov-kivi@users.noreply.github.com>
2026-08-20 08:21:12 +0300

    -

--- next.config.ts
+++ next.config.ts
@@ -1,72 +1,14 @@
 import type * as next          from 'next';
 import *      as fumadocs_next from 'fumadocs-mdx/next';
-import *      as config_site   from './configs/site.tsx';
 
 const withMDX = fumadocs_next.createMDX();
-const STATIC_CONTENT_CACHE_CONTROL = 'public, max-age=3600, s-maxage=86400, stale-while-revalidate=604800';
 
 const config: next.NextConfig = {
+  output: 'export',
   reactStrictMode: true,
   trailingSlash: false,
-
-  async headers() {
-    return [
-      {
-        source: '/docs/:path*',
-        headers: [{ key: 'Cache-Control', value: STATIC_CONTENT_CACHE_CONTROL }]
-      },
-      {
-        source: '/og/:path*',
-        headers: [{ key: 'Cache-Control', value: STATIC_CONTENT_CACHE_CONTROL }]
-      },
-      {
-        source: '/llms.txt',
-        headers: [{ key: 'Cache-Control', value: STATIC_CONTENT_CACHE_CONTROL }]
-      },
-      {
-        source: '/llms-full.txt',
-        headers: [{ key: 'Cache-Control', value: STATIC_CONTENT_CACHE_CONTROL }]
-      },
-      {
-        source: '/llms.mdx/:path*',
-        headers: [{ key: 'Cache-Control', value: STATIC_CONTENT_CACHE_CONTROL }]
-      },
-      {
-        source: '/sitemap.xml',
-        headers: [{ key: 'Cache-Control', value: STATIC_CONTENT_CACHE_CONTROL }]
-      },
-      {
-        source: '/robots.txt',
-        headers: [{ key: 'Cache-Control', value: STATIC_CONTENT_CACHE_CONTROL }]
-      },
-      {
-        source: '/opengraph-image',
-        headers: [{ key: 'Cache-Control', value: STATIC_CONTENT_CACHE_CONTROL }]
-      }
-    ];
-  },
-
-  async rewrites() {
-    return [
-      {
-        source: '/docs/:path*.mdx',
-        destination: '/llms.mdx/docs/:path*'
-      },
-    ];
-  },
-
-  async redirects() {
-    return Object.entries(config_site.info.social).map(([key, { href }]) => ({
-      source: '/:path*',
-      has: [
-        {
-          type: 'host' as const,
-          value: `${key}.vital-sandbox.com`
-        }
-      ],
-      destination: href.startsWith('http') ? href : `https://${href}`,
-      permanent: false
-    }));
+  images: {
+    unoptimized: true
   }
 };
 
