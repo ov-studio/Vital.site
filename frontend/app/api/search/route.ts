e8579456d4d5559e8bbc17e12c7e7614ef63e813
A <234397639+ov-kivi@users.noreply.github.com>
2026-08-20 09:05:24 +0300

    Create route.ts

---
+++ frontend/app/api/search/route.ts
@@ -0,0 +1,11 @@
+import * as lib_source             from '@/lib/source';
+import * as fumadocs_search_server from 'fumadocs-core/search/server';
+
+export const revalidate = false;
+
+const { staticGET } = fumadocs_search_server.createFromSource(lib_source.source, {
+    // https://docs.orama.com/docs/orama-js/supported-languages
+    language: 'english'
+});
+
+export { staticGET as GET };
