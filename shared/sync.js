80bc14318fd9555a640002163bc77c401fb30ca2
A <crazynuggets@outlook.com>
2026-08-25 18:51:06 +0300

    Update sync.js

--- shared/sync.js
+++ shared/sync.js
@@ -58,7 +58,6 @@ function update_gitignore(target_dir, rel_paths) {
 
 const CDN_TARGET_DIR = path.resolve(__dirname, '../frontend/public/cdn');
 const UI_SRC_DIR = path.resolve(__dirname, '../frontend/ui');
-
 const CDN_CSS_FILES = [
   { src: path.join(SHARED_DIR, 'app', 'theme.css'), dest: path.join(CDN_TARGET_DIR, 'theme.css') },
   { src: path.join(SHARED_DIR, 'app', 'global.css'), dest: path.join(CDN_TARGET_DIR, 'global.css') },
