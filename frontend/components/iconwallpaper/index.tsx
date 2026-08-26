327e2bc26bd6d5cb9483ee822d2f91a3d8d11994
A <234397639+ov-kivi@users.noreply.github.com>
2026-08-26 16:21:54 +0300

    Update index.tsx

--- frontend/components/iconwallpaper/index.tsx
+++ frontend/components/iconwallpaper/index.tsx
@@ -58,7 +58,7 @@ export function IconWallpaper({
   seed = 0,
   size = 36,
   gap = 100,
-  opacity = 0.2,
+  opacity = 0.1,
   vignette = true,
 }: IconWallpaperProps) {
   const wrapRef = react.useRef<HTMLDivElement>(null);
