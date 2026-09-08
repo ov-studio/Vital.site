10ef4456f170bfee6b8a878fad29432250de2098
A <48655846+ov-tron@users.noreply.github.com>
2026-09-08 20:08:30 +0300

    Update layout.shared.tsx

--- frontend/lib/layout.shared.tsx
+++ frontend/lib/layout.shared.tsx
@@ -1,4 +1,4 @@
-import * as component_brand        from '@/components/brand';
+import * as ui_brand               from '@/ui/brand';
 import * as fumadocs_layout_shared from 'fumadocs-ui/layouts/shared';
 
 export function baseOptions(): fumadocs_layout_shared.BaseLayoutProps {
@@ -6,7 +6,7 @@ export function baseOptions(): fumadocs_layout_shared.BaseLayoutProps {
     nav: {
       title: (
         <div style={{ marginTop: '12px' }}>
-          <component_brand.Brand size="xs" variant="full" href="/#"/>
+          <ui_brand.Brand size="xs" variant="full" href="/#"/>
         </div>
       )
     },
