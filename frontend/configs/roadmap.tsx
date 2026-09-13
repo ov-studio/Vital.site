d9405f8a49f7a5219e5ecc25d7906abbe358aac5
A <89867985+ov-aviril@users.noreply.github.com>
2026-09-13 21:53:09 +0300

    Update roadmap.tsx

--- frontend/configs/roadmap.tsx
+++ frontend/configs/roadmap.tsx
@@ -2431,8 +2431,10 @@ export const Roadmap: RoadmapSection[] = build([
           { label: 'Vehicle wheel config sync',                  status: 'completed' },
           { label: 'Vehicle wheel transform sync',               status: 'completed' },
           { label: 'Blendshape replication',                     status: 'pending'   },
-          { label: 'Animation state replication',                status: 'pending'   },
-          { label: 'Animation layer sync',                       status: 'pending'   }
+          { label: 'Animation state replication',                status: 'completed' },
+          { label: 'Animation layer sync',                       status: 'completed' },
+          { label: 'Late-join animation layer replay',           status: 'completed' },
+          { label: 'Animation bone-filter sync',                 status: 'completed' }
         ]
       },
       {
