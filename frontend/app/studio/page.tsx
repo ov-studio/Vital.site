e357a9b403f9d8f265678f5f715ad30471701f94
A <89867985+ov-aviril@users.noreply.github.com>
2026-09-18 21:37:58 +0300

    Update page.tsx

--- frontend/app/studio/page.tsx
+++ frontend/app/studio/page.tsx
@@ -1,27 +1,27 @@
 import '@/app/global.css';
-import * as component_overlay from '@/components/overlay';
-import * as component_navbar from '@/components/navbar';
-import * as component_footer from '@/components/footer';
-import * as component_studio from '@/components/studio';
+import * as component_overlay     from '@/components/overlay';
+import * as component_navbar      from '@/components/navbar';
+import * as component_footer      from '@/components/footer';
+import * as component_studio      from '@/components/studio';
 import * as component_clientshell from '@/components/clientshell';
-import * as next from 'next';
+import * as next                  from 'next';
 
 export const metadata: next.Metadata = {
-  title: 'Brand Studio',
+  title: 'Studio'
 };
 
 export default function StudioPage() {
   return (
     <component_clientshell.ClientShell>
-      <component_overlay.Overlay />
+      <component_overlay.Overlay/>
       <component_navbar.Navbar
         links={[
           { label: 'Documentations', href: '/docs' },
-          { label: 'Roadmap', href: '/roadmap' },
+          { label: 'Roadmap', href: '/roadmap' }
         ]}
       />
-      <component_studio.Studio />
-      <component_footer.Footer />
+      <component_studio.Studio/>
+      <component_footer.Footer/>
     </component_clientshell.ClientShell>
   );
 }
