be20e78e231b2c8153d3b13c176f17e23b443ef5
A <89867985+ov-aviril@users.noreply.github.com>
2026-09-23 12:07:11 +0300

    Update index.tsx

--- frontend/components/benchmarks/index.tsx
+++ frontend/components/benchmarks/index.tsx
@@ -109,7 +109,7 @@ export function Benchmarks() {
 
   return (
     <section id="benchmarks">
-      <ui_wallpaper.Wallpaper variant={2}/>
+      <ui_wallpaper.Wallpaper variant={4} color={"var(--rule)"}/>
       <div className="sw">
         <div className="page-head">
           <div className="sec-head sec-head--intro">
