55a513ba77f3ca7d43edf5c69a8546c903f25f92
A <89867985+ov-aviril@users.noreply.github.com>
2026-09-23 14:21:44 +0300

    Update index.tsx

--- frontend/components/workspace/index.tsx
+++ frontend/components/workspace/index.tsx
@@ -210,7 +210,7 @@ export function Workspace() {
 
   return (
     <main className="ws-page">
-      <ui_wallpaper.Wallpaper variant={5}/>
+      <ui_wallpaper.Wallpaper variant={7}/>
       <div className="sw">
         <div className="page-head">
           <div className="sec-head sec-head--intro">
