644a42ae3382f194d85c8a25bf228c850e2b7b13
A <89867985+ov-aviril@users.noreply.github.com>
2026-09-23 14:18:55 +0300

    -/-

--- frontend/components/benchmarks/index.tsx
+++ frontend/components/benchmarks/index.tsx
@@ -109,8 +109,7 @@ export function Benchmarks() {
 
   return (
     <section id="benchmarks">
-      <ui_wallpaper.Wallpaper variant={4} vignette={false}/>
-      
+      <ui_wallpaper.Wallpaper variant={5}/>
       <div className="sw">
         <div className="page-head">
           <div className="sec-head sec-head--intro">
--- frontend/components/roadmap/index.tsx
+++ frontend/components/roadmap/index.tsx
@@ -166,7 +166,7 @@ function SectionBlock({ section, index }: { section: config_roadmap.RoadmapSecti
 export function Roadmap() {
   return (
     <section id="roadmap">
-      <ui_wallpaper.Wallpaper variant={10} vignette={false}/>
+      <ui_wallpaper.Wallpaper variant={10}/>
       <div className="sw">
         <div className="page-head">
           <div className="sec-head sec-head--intro">
