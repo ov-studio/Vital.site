13ec629bf13d1b0a6646f414aa3c189055c885cc
A <89867985+ov-aviril@users.noreply.github.com>
2026-09-23 14:26:27 +0300

    Update index.tsx

--- frontend/components/vault/index.tsx
+++ frontend/components/vault/index.tsx
@@ -418,7 +418,7 @@ function VaultInner() {
   return (
     <>
       <section id="vault">
-      <ui_wallpaper.Wallpaper variant={4}/>
+      <ui_wallpaper.Wallpaper variant={11}/>
         <div className="sw">
           <VaultHead/>
 
