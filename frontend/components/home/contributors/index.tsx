a11e3f927f137b35d8f38c56ddaaf91a37db1f87
A <89867985+ov-aviril@users.noreply.github.com>
2026-09-23 11:20:08 +0300

    Removed type param

--- frontend/components/home/contributors/index.tsx
+++ frontend/components/home/contributors/index.tsx
@@ -26,7 +26,7 @@ export function Contributors() {
 
   return (
     <section id="contributors">
-      <ui_wallpaper.Wallpaper type="line" variant={1} vignette={false}/>
+      <ui_wallpaper.Wallpaper variant={1} vignette={false}/>
       <div className="sw">
         <div className="sec-head">
           <div className="rev">
--- frontend/components/home/hero/index.tsx
+++ frontend/components/home/hero/index.tsx
@@ -44,7 +44,7 @@ export function Hero() {
 
   return (
     <section id="hero">
-      <ui_wallpaper.Wallpaper type="line" variant={6} vignette={true}/>
+      <ui_wallpaper.Wallpaper variant={6} vignette={true}/>
       <div className="hcorner hcorner-tl"/>
       <div className="hcorner hcorner-tr"/>
       <div className="hcorner hcorner-bl"/>
